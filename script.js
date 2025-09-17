bb7f955fa72d0230d4f16aa548a9df28123b4acc
p14082003 <ptaquino2003@gmail.com>
2025-09-17 18:43:45 -0300

    Avance Nro1 - Header y main listos, en su mayoría

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-let clicked = false;
-
-function button1() {
-  clicked = !clicked;
-  if (clicked) {
-    document.getElementById("hola").style = "color: green";
-  } else {
-    document.getElementById("hola").style = "color: red";
-  }
-}
+// Mandar mail?
