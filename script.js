e4afdfa4ae6894bb74ca8973c9721b46e634223f
p14082003 <ptaquino2003@gmail.com>
2025-09-18 18:41:41 -0300

    Estructuración - Faltan imagenes proyectos, y acomodar el form del mail

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Mandar mail?
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("openbtn").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0px";
+  document.getElementById("openbtn").style.display = "block";
+}
