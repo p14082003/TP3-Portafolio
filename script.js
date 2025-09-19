0d81b8513dce92dcd9df9911bca7c9309c436bcb
p14082003 <ptaquino2003@gmail.com>
2025-09-19 17:04:12 -0300

    tentantiva de entrega #1 - falta comentar todo el codigo

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("openbtn").style.display = "none";
+  document.getElementById("lateral").style.width = "13%";
+  document.getElementById("boton-abrir").style.display = "none";
 }
 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0px";
-  document.getElementById("openbtn").style.display = "block";
+  document.getElementById("lateral").style.width = "0px";
+  document.getElementById("boton-abrir").style.display = "block";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".snap-section");
+  const navLinks = document.querySelectorAll(".lateral-link");
+
+  const sectionToLink = {};
+  navLinks.forEach((link) => {
+    const targetId = link.getAttribute("href").substring(1);
+    sectionToLink[targetId] = link;
+  });
+
+  const scrollContainer = document.querySelector(".scroll-container");
+
+  scrollContainer.addEventListener("scroll", () => {
+    let current = "";
+    console.log(current);
+
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+        current = section.id;
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.style.color = "white";
+    });
+
+    if (current && sectionToLink[current]) {
+      sectionToLink[current].style.color = getComputedStyle(document.documentElement).getPropertyValue("--colorDestaque").trim();
+    }
+  });
+
+  //Form
+  const messageButton = document.getElementById("form-boton-enviar");
+  const messageBox = document.getElementById("confirmacion");
+  const form = document.getElementById("form-contacto");
+
+  messageButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (form.checkValidity()) {
+      console.log("test");
+      messageBox.classList.add("show");
+
+      setTimeout(() => {
+        messageBox.classList.remove("show");
+      }, 3000);
+
+      const inputs = document.querySelectorAll(".form-campo");
+      inputs.forEach((input) => {
+        input.value = "";
+      });
+
+      form.submit();
+    }
+  });
+});
