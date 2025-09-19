957c049a7496408bb47f95d2bc30a70b50412150
p14082003 <ptaquino2003@gmail.com>
2025-09-19 18:02:12 -0300

    otro intento! - En el live preview no pasaba!

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const messageBox = document.getElementById("confirmacion");
   const form = document.getElementById("form-contacto");
 
-  messageButton.addEventListener("click", (e) => {
-    e.preventDefault();
+  messageButton.addEventListener("click", () => {
+    //e.preventDefault();
     if (form.checkValidity()) {
       console.log("test");
       messageBox.classList.add("show");
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
         input.value = "";
       });
 
-      form.submit();
+      //form.submit();
     }
   });
 });
