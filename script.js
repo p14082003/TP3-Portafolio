5a6c4da43abf2f42e7de5ec7cf55712b4fcf6ae3
p14082003 <ptaquino2003@gmail.com>
2025-09-19 17:52:54 -0300

    intento arreglo: si hago preventDefault no me da error en campo invalido

--- script.js
+++ script.js
@@ -46,7 +46,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("form-contacto");
 
   messageButton.addEventListener("click", (e) => {
-    e.preventDefault();
     if (form.checkValidity()) {
       console.log("test");
       messageBox.classList.add("show");
